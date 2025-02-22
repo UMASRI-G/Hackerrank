1b18c4ba1649f91f9249f3cf67a807dd48ac91a8
UMASRI-G <gumasri24@gmail.com>
2025-02-22 21:49:12 +0530

    Stdin & Stdout

---
+++ stdinout.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        // Complete this line
+        // Complete this line
+
+        System.out.println(a);
+        // Complete this line
+        // Complete this line
+        int b = scan.nextInt();
+        System.out.println(b);
+        int c = scan.nextInt();
+        System.out.println(c);
+    }
+}
